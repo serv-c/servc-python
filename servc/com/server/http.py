677f16f9dbeb78fefd6e8ce07de7fe4e11a73172
Yusuf Ali <ali@yusuf.email>
2024-10-08 04:28:26 +0000

    chore: fixed lint issues

--- servc/com/server/http.py
+++ servc/com/server/http.py
@@ -1,6 +1,6 @@
 import os
-from multiprocessing import Process
 import uuid
+from multiprocessing import Process
 
 from flask import Flask, jsonify, request
 
@@ -99,7 +99,7 @@ class HTTPInterface(ServiceComponent):
             body = request.json
             if body and body["route"] and body["inputs"]:
                 force = False if "force" not in body else body["force"]
-                no_id = ("id" not in body) or ("id" in body and body["id"] in ("","0"))
+                no_id = ("id" not in body) or ("id" in body and body["id"] in ("", "0"))
 
                 value: InputPayload = {
                     "id": str(uuid.uuid4()) if no_id else body["id"],
